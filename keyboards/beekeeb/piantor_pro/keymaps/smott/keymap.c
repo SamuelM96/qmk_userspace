a94ae25de638821e38b207fd8ef2f58f6f95c475
Samuel Mott <samuel.e.mott@gmail.com>
2025-09-21 17:04:05 +0100

    Added more function keys to game layer

--- keyboards/beekeeb/piantor_pro/keymaps/smott/keymap.c
+++ keyboards/beekeeb/piantor_pro/keymaps/smott/keymap.c
@@ -102,9 +102,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   ),
 
     [_GAME2] = LAYOUT(
-      KC_F8,  KC_GRAVE, KC_1, KC_2, KC_3, KC_T,                                  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-      KC_F5,  KC_TRNS,  KC_4, KC_5, KC_6, KC_G,                                  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-      KC_ENT, KC_0,     KC_7, KC_8, KC_9, KC_B,                                  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, QWERTY,
+      KC_F8,  KC_GRAVE, KC_1, KC_2, KC_3, KC_T,                                  KC_F12, KC_F7, KC_F8, KC_F9, KC_TRNS, KC_TRNS,
+      KC_F5,  KC_TRNS,  KC_4, KC_5, KC_6, KC_G,                                  KC_F11, KC_F4, KC_F5, KC_F6, KC_TRNS, KC_TRNS,
+      KC_ENT, KC_0,     KC_7, KC_8, KC_9, KC_B,                                  KC_F10, KC_F1, KC_F2, KC_F3, KC_TRNS, QWERTY,
                               KC_TRNS, KC_TRNS, KC_SPC,                 KC_TRNS, KC_TRNS, KC_TRNS
   ),
 
