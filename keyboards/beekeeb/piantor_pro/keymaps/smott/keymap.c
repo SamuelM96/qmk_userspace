8ade099e3b35fd333d5c87d4e7fe39f5aed27fb0
Samuel Mott <samuel.e.mott@gmail.com>
2026-03-13 18:11:01 +0000

    Tweaks to OSMs

--- keyboards/beekeeb/piantor_pro/keymaps/smott/keymap.c
+++ keyboards/beekeeb/piantor_pro/keymaps/smott/keymap.c
@@ -31,38 +31,28 @@ enum layers
 #define SPC_NAV LT(_NAV, KC_SPC)
 #define NUM_ESC LT(_NUM, KC_ESC)
 
-#define OSM_LGUI OSM(MOD_LGUI)
-#define OSM_LCTL OSM(MOD_LCTL)
-#define OSM_LALT OSM(MOD_LALT)
-#define OSM_LSFT OSM(MOD_LSFT)
-
-#define OSM_RGUI OSM(MOD_RGUI)
-#define OSM_RCTL OSM(MOD_RCTL)
-#define OSM_RALT OSM(MOD_RALT)
-#define OSM_RSFT OSM(MOD_RSFT)
-
 #define TMUX_LDR LCTL(KC_A)
 
 // clang-format off
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_QWERTY] = LAYOUT(
-      KC_LGUI,    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                         KC_Y,    KC_U,    KC_I,    KC_O,   KC_P,   KC_BSPC,
+      KC_LGUI,    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                         KC_Y,    KC_U,    KC_I,    KC_O,   KC_P,   KC_LALT,
       KC_LSFT,    KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                         KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN,  KC_QUOT,
-      KC_LCTL,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                         KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH,  KC_ESC,
-                                          KC_LCTL, SPC_NAV, NUM_ESC,        SYM, KC_LSFT, KC_LALT
+      KC_LCTL,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                         KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH,  OS_LCA,
+                                          OS_LCTL, SPC_NAV, NUM_ESC,       SYM,  OS_LSFT, OS_LALT
   ),
 
     [_NAV] = LAYOUT(
-      KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS, KC_ESC,                     KC_TRNS, KC_HOME, KC_PGDN, KC_PGUP,   KC_END, KC_TRNS,
-      KC_TRNS, OSM_LGUI, OSM_LALT, OSM_LSFT, OSM_LCTL, KC_ENT,                     KC_TAB,  KC_LEFT, KC_DOWN, KC_UP,    KC_RIGHT,TMUX_LDR,
+      KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  QK_REP,  KC_ESC,                     KC_TRNS, KC_HOME, KC_PGDN, KC_PGUP,   KC_END, KC_TRNS,
+      KC_TRNS,  OS_LGUI,  OS_LALT,  OS_LSFT, OS_LCTL,  KC_ENT,                     KC_TAB,  KC_LEFT, KC_DOWN, KC_UP,    KC_RIGHT,TMUX_LDR,
       KC_TRNS,  KC_CAPS,  KC_TRNS,  KC_TRNS, C(KC_SPC),KC_SPC,                     KC_DEL,  KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS, KC_TRNS,
                                                 OTHER, KC_TRNS,  KC_SPC,      MEDIA, KC_BSPC, KC_TRNS
   ),
 
     [_NUM] = LAYOUT(
       KC_TRNS, UK_LBRC,  UK_LABK,  UK_MINS,  UK_RABK,  UK_RBRC,                    TMUX_LDR,   KC_7, KC_8, KC_9, KC_UP,   KC_TRNS,
-      KC_TRNS, OSM_LGUI, OSM_LALT, OSM_LSFT, OSM_LCTL, UK_RCBR,                    KC_SLSH,    KC_4, KC_5, KC_6, KC_DOWN, TMUX_LDR,
-      KC_TRNS, UK_BSLS,  UK_ASTR,  UK_COLN,  UK_PLUS,  UK_UNDS,                       KC_X,    KC_1, KC_2, KC_3, UK_DOT,  GAME,
+      KC_TRNS, OS_LGUI,  OS_LALT,  OS_LSFT,  OS_LCTL,  UK_RCBR,                    KC_SLSH,    KC_4, KC_5, KC_6, KC_DOWN, TMUX_LDR,
+      KC_TRNS, UK_BSLS,  UK_ASTR,  UK_COLN,  UK_PLUS,  UK_UNDS,                       KC_X,    KC_1, KC_2, KC_3, UK_DOT,  KC_TRNS,
                                              KC_TRNS,  OTHER, KC_TRNS,       KC_0, KC_BSPC,    KC_ENT
   ),
 
@@ -75,8 +65,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     [_FUNC] = LAYOUT(
       KC_TRNS, KC_F13,   KC_F14,   KC_F15,   KC_F16,   KC_F17,                     KC_F12, KC_F7, KC_F8, KC_F9, KC_PSCR, KC_TRNS,
-      KC_TRNS, OSM_LGUI, OSM_LALT, OSM_LSFT, OSM_LCTL, KC_TRNS,                    KC_F11, KC_F4, KC_F5, KC_F6, KC_INS,  KC_TRNS,
-      KC_TRNS, OSM_RGUI, OSM_RALT, OSM_RSFT, OSM_RCTL, KC_TRNS,                    KC_F10, KC_F1, KC_F2, KC_F3, KC_TRNS, KC_TRNS,
+      KC_TRNS, OS_LGUI,  OS_LALT,  OS_LSFT,  OS_LCTL,  KC_TRNS,                    KC_F11, KC_F4, KC_F5, KC_F6, KC_INS,  KC_TRNS,
+      KC_TRNS, OS_RGUI,  OS_RALT,  OS_RSFT,  OS_RCTL,  KC_TRNS,                    KC_F10, KC_F1, KC_F2, KC_F3, KC_TRNS, KC_TRNS,
                                               KC_TRNS, KC_TRNS, KC_TRNS,    KC_TRNS,KC_TRNS, KC_TRNS
   ),
 
@@ -90,7 +80,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_OTHER] = LAYOUT(
       KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                      KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, QK_BOOT, KC_TRNS,
       KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                      KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-      KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                      KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+      KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                      KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, GAME,
                                           KC_TRNS, KC_TRNS,  KC_SPC,     KC_ENT, KC_TRNS, KC_TRNS
   ),
 
